18b6b2041a4685f1c50a78db855e3c702cce1c3d
Juliancio Carvalho <juliancio_alves@hotmail.com>
2023-10-05 22:10:20 -0300

    desafio carros volvo

--- carousel-veiculos-volvo/script.js
+++ carousel-veiculos-volvo/script.js
@@ -1,12 +1,45 @@
+const tabAllModels = document.getElementById("tabAllModels")
+const tabSuv = document.getElementById("tabSuv")
+const tabCrossover = document.getElementById("tabCrossover")
+
 const btnPrevious = document.getElementById("btnPrevious")
 const btnNext = document.getElementById("btnNext")
 
 const listCards = document.getElementById("listCards")
 
 btnNext.onclick = () => {
-    listCards.scrollLeft += 60
+    listCards.scrollLeft += 110
+
+    if (listCards.scrollLeft === (listCards.scrollWidth - listCards.clientWidth)) {
+        btnNext.classList.add("btn-disabled")
+    }
 }
 
 btnPrevious.onclick = () => {
-    listCards.scrollLeft -= 60
+    listCards.scrollLeft -= 110
+
+    if (listCards.scrollLeft === (listCards.clientWidth - listCards.scrollWidth)) {
+        btnPrevious.classList.add("btn-disabled")
+    }
+}
+
+tabAllModels.onclick = () => {
+    tabAllModels.classList.add("active")
+
+    tabSuv.classList.remove("active")
+    tabCrossover.classList.remove("active")
+}
+
+tabSuv.onclick = () => {
+    tabSuv.classList.add("active")
+
+    tabAllModels.classList.remove("active")
+    tabCrossover.classList.remove("active")
+}
+
+tabCrossover.onclick = () => {
+    tabCrossover.classList.add("active")
+
+    tabSuv.classList.remove("active")
+    tabAllModels.classList.remove("active")
 }
